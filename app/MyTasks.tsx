5f5cd8932d2f57a7e38d4feb1384a0d0c60b2d71
Bogdan Bucur <bogdanrbucur@gmail.com>
2024-07-13 14:50:36 +0300

    fix: my tasks taking too much space if empty

--- app/MyTasks.tsx
+++ app/MyTasks.tsx
@@ -2,9 +2,9 @@ import SingleUserTasks from "@/components/SingleUserTasks";
 import { Task } from "@prisma/client";
 
 function MyTasks({ tasks, hasSubordinates }: { tasks: Task[]; hasSubordinates: boolean }) {
-	const title = tasks.length == 0 ? `No open tasks` : tasks.length == 1 ? `My open task` : `My ${tasks.length} open tasks`;
+	const title = tasks.length == 0 ? `No open tasks 👌` : tasks.length == 1 ? `My open task` : `My ${tasks.length} open tasks`;
 	return (
-		<div id="my-tasks" className="flex-grow h-5/6 overflow-auto border-none p-3 pr-0 space-y-2 md:px-6 md:pr-0">
+		<div id="my-tasks" className={`flex-grow h-${tasks.length == 0 ? "1/6" : "full"} overflow-auto border-none p-3 pr-0 space-y-2 md:px-6 md:pr-0`}>
 			<div className="p-0">
 				<h4 className="scroll-m-20 text-xl font-semibold tracking-tight">{title}</h4>
 			</div>
