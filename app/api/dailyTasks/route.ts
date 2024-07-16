cb13bcbff9eb6a037ada77ef0108479e6e103307
Bogdan Bucur <bogdanrbucur@gmail.com>
2024-07-16 17:33:17 +0300

    feat: daily check expired tokens

--- app/api/dailyTasks/route.ts
+++ app/api/dailyTasks/route.ts
@@ -1,7 +1,7 @@
 import { sendEmail } from "@/app/email/email";
 import prisma from "@/prisma/client";
-import { NextRequest, NextResponse } from "next/server";
 import { subDays } from "date-fns";
+import { NextRequest, NextResponse } from "next/server";
 
 const dueSoonDays = process.env.DUE_SOON_DAYS ? parseInt(process.env.DUE_SOON_DAYS) : 10;
 
@@ -16,7 +16,8 @@ export async function POST(req: NextRequest) {
 
 	console.log(`Overdue tasks retrieved: ${overdueTasks.length}...`);
 	// Change status to overdue (5) if task is past due
-	overdueTasks.forEach(async (task) => {
+
+	for (const task of overdueTasks) {
 		console.log(`Task ${task.id} is overdue!`);
 
 		// send email notification to assignee and their manager
@@ -31,7 +32,7 @@ export async function POST(req: NextRequest) {
 			where: { id: task.id },
 			data: { statusId: 5 },
 		});
-	});
+	}
 
 	// Check for due today tasks...
 	const dueSoonTasks = await prisma.task.findMany({
@@ -43,24 +44,54 @@ export async function POST(req: NextRequest) {
 
 	console.log(`Tasks due soon retrieved: ${dueSoonTasks.length}...`);
 
-	dueSoonTasks.forEach(async (task) => {
+	for (const task of dueSoonTasks) {
 		console.log(`Task ${task.id} is due soon!`);
 
-		// send email notification to assignee and their manager
-		await sendEmail({
-			recipients: task.assignedToUser ? task.assignedToUser.email : "",
-			emailType: "taskDueSoon",
-			task,
+		try {
+			// send email notification to assignee and their manager
+			await sendEmail({
+				recipients: task.assignedToUser ? task.assignedToUser.email : "",
+				emailType: "taskDueSoon",
+				task,
+			});
+
+			await prisma.task.update({
+				where: { id: task.id },
+				data: { dueSoonReminderSent: true },
+			});
+		} catch (err) {
+			console.error(err);
+		}
+	}
+
+	// Check for expired password reset tokens and delete them
+	const usersWithExpiredTokens: Set<string> = new Set();
+	try {
+		const tokens = await prisma.passwordResetToken.findMany({
+			where: { expiresAt: { lte: new Date() } },
 		});
 
-		await prisma.task.update({
-			where: { id: task.id },
-			data: { dueSoonReminderSent: true },
-		});
+		console.log(`${tokens.length} expired password reset tokens found`);
+
+		for (const token of tokens) {
+			await prisma.passwordResetToken.delete({ where: { id: token.id } });
+			usersWithExpiredTokens.add(token.userId);
+		}
+	} catch (err) {
+		console.log(err);
+	}
+
+	// TODO Filter users with expired tokens and keep only the unverified ones
+	const allUnverifiedUsers = await prisma.user.findMany({
+		where: { status: "unverified" },
 	});
 
-	// TODO check for expired password reset tokens
-	//
+	const unverifiedUsersWithExpiredTokens = usersWithExpiredTokens.forEach((userId) => {
+		allUnverifiedUsers.find((user) => user.id === userId)?.id;
+	});
+
+	// console.log("Unverified users found: ", allUnverifiedUsers.length);
+	// console.log("Unverified users with expired tokens found: ", unverifiedUsersWithExpiredTokens.length);
 
 	return NextResponse.json({ ok: true });
 }
