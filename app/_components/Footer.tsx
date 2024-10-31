41f4d52bf22f3dc7ea25dde28858f87c8924fc4c
Bogdan Bucur <bogdanrbucur@gmail.com>
2024-10-31 15:45:22 +0200

    fix: footer version and deoployment UI adjustments

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -7,8 +7,8 @@ export default function Footer() {
 	return (
 		<footer className="flex items-center h-7 px-4">
 			<div className="flex text-left text-xs text-muted-foreground items-center">
-				<svg height="10" width="10" className="mr-2">
-					<circle cx="5" cy="5" r="5" fill={deployment} />
+				<svg height="9" width="9" className="mr-1">
+					<circle cx="4" cy="4" r="4" fill={deployment} />
 				</svg>
 				{version}
 			</div>
