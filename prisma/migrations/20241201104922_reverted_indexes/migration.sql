34a093548ccd316c5d9f08addfd8e146aabd58af
Bogdan Bucur <bogdanrbucur@gmail.com>
2024-12-01 12:58:47 +0200

    improved indexes

---
+++ prisma/migrations/20241201104922_reverted_indexes/migration.sql
@@ -0,0 +1,32 @@
+-- DropIndex
+DROP INDEX "Department_name_idx";
+
+-- DropIndex
+DROP INDEX "Department_id_idx";
+
+-- DropIndex
+DROP INDEX "Status_displayName_idx";
+
+-- DropIndex
+DROP INDEX "Status_id_idx";
+
+-- DropIndex
+DROP INDEX "User_managerId_idx";
+
+-- DropIndex
+DROP INDEX "User_lastName_idx";
+
+-- DropIndex
+DROP INDEX "User_firstName_idx";
+
+-- DropIndex
+DROP INDEX "User_isAdmin_idx";
+
+-- DropIndex
+DROP INDEX "User_active_idx";
+
+-- DropIndex
+DROP INDEX "User_status_idx";
+
+-- DropIndex
+DROP INDEX "User_departmentId_idx";
